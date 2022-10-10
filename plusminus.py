9d6ebd15753b1a4f9f8b1bca08568c37cfee410c
Jessie <114779693+jessie-black@users.noreply.github.com>
2022-10-10 15:38:25 -0700

    Update plusminus.py
    Simplified declarations & initializations to single line
    Compacted if/elif/else statements to one line each (needed to double check to make sure this is ok in python as well as java)
    Fixed issue of trailing zero with use of formatting in print statement 
    Simplified print statement to 1 line within for loop

--- plusminus.py
+++ plusminus.py
@@ -1,34 +1,29 @@
+# HACKERRANK PROBLEM: Plus Minus -- Given an array of integers, calculate the ratios of its elements that are positive,
+# negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
+
 import math
 import os
 import random
 import re
 import sys
 
-# HACKERRANK PROBLEM: Plus Minus -- Given an array of integers, calculate the ratios of its elements that are positive,
-# negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
-
 def plusMinus(arr):
     ## Given INTEGER_ARRAY arr with up to 100 elements
     ## with values between -100 and 100, print ratio of
     ## positive integers, negative integers, and zero.
     
-    pos = 0
-    neg = 0 
-    zero = 0
-    total = 0
+    # Initialize counters
+    pos = neg = zero = total = 0
     
     for num in arr:
-        if num > 0: 
-            pos = pos +1
-        elif num < 0: 
-            neg = neg + 1
-        else:
-            zero = zero +1
+        if num > 0: pos = pos +1
+        elif num < 0: neg = neg + 1
+        else: zero = zero +1
         total = total + 1
     
-    print(round(pos/total,6))
-    print(round(neg/total,6))
-    print(round(zero/total,6))    
+    # Print results as proportion with 6 digits after decimal point
+    for x in (pos,neg,zero):
+        print(f"{(x/total):.6f}")
 
 if __name__ == '__main__':
     n = int(input().strip())
