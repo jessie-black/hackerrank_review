be1e3c98ca3543f7d5863049e29df4cbf1458439
Jessie <114779693+jessie-black@users.noreply.github.com>
2022-10-11 09:14:40 -0700

    Update lonely_integer.py
    Added alternative solutions for improved performance

--- lonely_integer.py
+++ lonely_integer.py
@@ -27,3 +27,18 @@ if __name__ == '__main__':
 
     fptr.close()
 ## SCORE RECEIVED: 100.00 / ACCEPTED
+
+## My solution works, but requires iterating through the entire array O(n) in all cases. An alternative is to 
+# check how many occurrences each element has. It will be substantially faster if element occurs sooner in array. 
+#
+# def lonelyinteger(a): 
+#    for x in a: if a.count(x) == 1 : return x
+
+## More elegant solution posted by johannzen based on logic that if you double the sum of the SET 
+# (one of each number, including the unique number) and then subtract the sum of the ARRAY (with 
+# duplicates), you are left with the value of the unique element.
+#
+# def lonelyinteger(a):
+#    sum_of_set = sum(set(a))
+#    sum_of_list = sum(a)
+#   return sum_of_set*2-sum_of_list
